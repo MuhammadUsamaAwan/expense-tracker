eb02f83b41577b9b7ca494d64910fd6ef9651656
MuhammadUsamaAwan <underusama@gmail.com>
2024-06-03 05:50:56 +0500

    fix: validatiion trim

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 
 export const authSchema = z.object({
-  username: z.string().min(5).max(30).trim(),
-  password: z.string().min(5).max(30).trim(),
+  username: z.string().trim().min(5).max(30),
+  password: z.string().trim().min(5).max(30),
 });
 
 export const categorySchema = z.object({
-  name: z.string().min(3).max(30).trim(),
-  color: z.string().min(1).trim(),
+  name: z.string().trim().min(3).max(30),
+  color: z.string().trim().min(1),
 });
 
 export const updateCategorySchema = categorySchema.extend({
@@ -17,7 +17,7 @@ export const updateCategorySchema = categorySchema.extend({
 export const expenseSchema = z.object({
   amount: z.number().positive(),
   category: z.string().uuid(),
-  description: z.string().max(150).trim().optional(),
+  description: z.string().trim().max(150).optional(),
   date: z.date(),
 });
 
